4619ec3aed0cea99556e8410deb028ec1262d7d5
Matheus <matheusnagot@hotmail.com>
2022-04-25 11:13:59 -0300

    feat:new fn create a list/deck of cards

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,4 +2,13 @@ defmodule Cards do
   @moduledoc """
   Documentation for `Cards`.
   """
+
+  @doc """
+    create deck cards, an array of playing cards
+  """
+
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
+
 end
