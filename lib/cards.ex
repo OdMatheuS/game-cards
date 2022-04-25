e3410f358cbb3898a4b339a369eded307db2f3bb
Matheus <matheusnagot@hotmail.com>
2022-04-25 11:11:58 -0300

    feat:first module + doc

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,17 +2,4 @@ defmodule Cards do
   @moduledoc """
   Documentation for `Cards`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
