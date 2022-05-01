06bb1a05abd2667d62be0538e33e2096d7db52f1
Matheus <matheusnagot@hotmail.com>
2022-05-01 19:48:06 -0300

    docs:add example and description to fn deal()

--- lib/cards.ex
+++ lib/cards.ex
@@ -26,6 +26,16 @@ defmodule Cards do
     |> Enum.member?(card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in the hand.
+
+## Examples
+    iex> deck = Cards.create_deck
+    iex> {hand, deck} = Cards.deal(deck,2)
+    iex> hand
+    ["Ace of Diamonds", "Ace of Hearts"]
+"""
   def deal(deck, hand_size) do
     deck
     |> Enum.split(hand_size)
