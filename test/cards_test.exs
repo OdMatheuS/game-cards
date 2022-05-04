15bb41007d31cb22edb69e563b5ce60e1385c49c
Matheus <matheusnagot@hotmail.com>
2022-05-04 18:07:31 -0300

    style: add () to fn length in test file

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -3,14 +3,12 @@ defmodule CardsTest do
   doctest Cards
 
   test "create_deck makes 40 cards" do
-    deck_lenght = Cards.create_deck() |> length
+    deck_lenght = Cards.create_deck() |> length()
     assert deck_lenght == 40
   end
 
   test "shuffling a deck randomizes it" do
-    deck = Cards.create_deck
+    deck = Cards.create_deck()
     assert deck != Cards.shuffle(deck)
-    
   end
-
 end
