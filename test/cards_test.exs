693c8f9aeef82d5300420c42fff193d24a8cf4b3
Matheus <matheusnagot@hotmail.com>
2022-05-04 17:59:26 -0300

    test:created first test, card deck size validation

--- test/cards_test.exs
+++ test/cards_test.exs
@@ -1,4 +1,9 @@
 defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
+
+  test "create_deck makes 40 cards" do
+    deck_lenght = Cards.create_deck() |> length
+    assert deck_lenght == 40
+  end
 end
